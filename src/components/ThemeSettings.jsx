1c000d6546881a903ca41a36d63a57365470f747
Tania Khan <ShineKhan@Shines-MacBook-Pro.local>
2022-06-23 23:42:11 -0400

    setup all components

---
+++ src/components/ThemeSettings.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ThemeSet() {
+  return (
+    <div>ThemeSet</div>
+  )
+}
+
+export default ThemeSet
---
+++ src/pages/Charts/ColorMapping.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ColorMapping() {
+  return (
+    <div>ping</div>
+  )
+}
+
+export default ColorMapping;
